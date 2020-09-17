5c0bd06b25a4a21d51c4115f3aebabd410f00094
votiendung <69288572+votiendung@users.noreply.github.com>
2020-09-17 16:48:35 +0700

    add

---
+++ Chicken.java
@@ -0,0 +1,10 @@
+public class Chicken extends Animal implements Edible{
+    @Override
+    public String makeSound() {
+        return "Chicken: cluck-cluck!";
+    }
+    @Override
+    public String howToEat() {
+        return "could be fried";
+    }
+}
